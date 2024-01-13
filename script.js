dc518f180f21eb37b609b769fa399fbf7f778517
Shreyaskr8 <skrravi08@gmail.com>
2024-01-13 19:27:20 +0530

    password rules appearing changes

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const password1 = document.getElementById("password");
 const password2 = document.getElementById("confirm-password");
 
+password1.addEventListener("focusin", showPasswordRules)
+password1.addEventListener("focusout", hidePasswordRules)
 password2.addEventListener("input", validate);
+
+function hidePasswordRules()
+{
+    const doc = document.querySelector(".password-rules");
+    doc.style.display = "none";
+}
+
+function showPasswordRules()
+{
+    const doc = document.querySelector(".password-rules");
+    doc.style.display = "block";
+
+}
+
 function validate()
 {
     if(!password1.value.includes(password2.value))
